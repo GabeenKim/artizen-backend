4ffa1ef1895c3a19270f73d35cbb295d69938fae
JoWonHyeong <mesh153@naver.com>
2023-09-08 18:08:42 +0900

    [update] review, ticket dao 작성

--- src/main/java/com/service/spring/dao/ReviewsDAO.java
+++ src/main/java/com/service/spring/dao/ReviewsDAO.java
@@ -1,5 +1,10 @@
 package com.service.spring.dao;
 
-public interface ReviewsDAO {
+import java.util.List;
 
-}
+import com.service.spring.vo.Reviews;
+
+public interface ReviewsDAO {
+	int addReview(int userId, int contentId, String review, int score); //P14.리뷰작성
+	List<Reviews> showReview(int writerId); //P15.작가 리뷰 목록
+}	
