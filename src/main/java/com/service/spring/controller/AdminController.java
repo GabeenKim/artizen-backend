9e8af7bf34b7e6f003453b352190763447e06b87
JoWonHyeong <mesh153@naver.com>
2023-09-15 10:09:21 +0900

    [feature] admin 기능추가

---
+++ src/main/java/com/service/spring/controller/AdminController.java
@@ -0,0 +1,78 @@
+package com.service.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.service.spring.dao.AdminDAO;
+import com.service.spring.service.AdminService;
+import com.service.spring.vo.Contents;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired
+	private AdminService adminService;
+	
+	@GetMapping("/okList")
+	public ResponseEntity okList() throws Exception{
+		try {
+			List<Contents> contents = adminService.okList();
+			return new ResponseEntity(contents, HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+	}
+
+	@GetMapping("/failList")
+	public ResponseEntity failList() throws Exception{
+		try {
+			List<Contents> contents = adminService.failList();
+			return new ResponseEntity(contents, HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@GetMapping("/checkList")
+	public ResponseEntity checkList() throws Exception{
+		try {
+			List<Contents> contents = adminService.checkList();
+			return new ResponseEntity(contents, HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@GetMapping("/ok/{contentId}")
+	public ResponseEntity ok(@PathVariable int contentId) throws Exception{
+		try {
+			adminService.ok(contentId);
+			return new ResponseEntity(HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@GetMapping("/fail/{contentId}")
+	public ResponseEntity fail(@PathVariable int contentId) throws Exception{
+		try {
+			adminService.fail(contentId);
+			return new ResponseEntity(HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	
+}
---
+++ src/main/java/com/service/spring/service/AdminService.java
@@ -0,0 +1,15 @@
+package com.service.spring.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.service.spring.vo.Contents;
+
+public interface AdminService {
+	List<Contents> checkList() throws Exception;
+	int ok(int contentId) throws Exception; 
+	int fail(int contentId) throws Exception;
+	List<Contents> okList() throws Exception;
+	List<Contents> failList() throws Exception;
+	List<Map<String,Object>> userList() throws Exception;
+}
